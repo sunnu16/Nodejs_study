77bd7610797fed6d3f82498d637421c7987ad09d
Ubuntu <ubuntu@ip-172-31-34-210.ap-northeast-2.compute.internal>
2022-07-28 18:06:49 +0000

    fs.readdir Nodejs에서 파일목록 알아내기

---
+++ read_dir/readdir.js
@@ -0,0 +1,10 @@
+//fs.readdir - file list in directory
+
+var testFoler = '../data/';
+var fs = require('fs');
+
+fs.readdir(testFoler, function(error, filelist){
+    console.log(filelist); 
+    //data 디렉토리의 위치 확인 후 실행(실행을 어디서 하느냐에 따라 var testFoler의 경로 설정)
+})
+
