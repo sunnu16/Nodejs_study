fce5032c8d3976a08d33708c77759e3b4a837d16
Ubuntu <ubuntu@ip-172-31-34-210.ap-northeast-2.compute.internal>
2022-07-28 16:36:50 +0000

    syntax2

---
+++ syntax/compersion.js
@@ -0,0 +1,9 @@
+//비교 연산자
+
+console.log(1+1); //이항연산자
+
+console.log(1==1); //true 좌항과 우항을 비교하는 표현식
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1===1); //true
+console.log(1===2); //false
