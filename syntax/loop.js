fe2107ff636bd5a141723cfb6b9b6025e8c6394d
Ubuntu <ubuntu@ip-172-31-34-210.ap-northeast-2.compute.internal>
2022-07-28 17:45:37 +0000

    syntax3

---
+++ syntax/loop.js
@@ -0,0 +1,15 @@
+//Loop - 반복문
+
+//while(true){내용} <- true 조건이 아닐떄까지 내용을 반복 실행
+
+console.log('A');
+console.log('B');
+
+var i = 0;
+while (i < 2) {
+    console.log('C1');
+    console.log('C2');
+    i = i + 1;
+}
+
+console.log('D');
