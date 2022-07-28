c9974a7fe1020d57ffd68bbccd2d1be09da54cf5
sunnu16 <70008149+sunnu16@users.noreply.github.com>
2022-07-29 03:08:14 +0900

    Update readdir.js

--- read_dir/readdir.js
+++ read_dir/readdir.js
@@ -5,6 +5,6 @@ var fs = require('fs');
 
 fs.readdir(testFoler, function(error, filelist){
     console.log(filelist); 
-    //data 디렉토리의 위치 확인 후 실행(실행을 어디서 하느냐에 따라 var testFoler의 경로 설정)
+    //data 디렉토리의 위치 확인 후 실행(실행을 어디서 하느냐에 따라 data의 경로 설정)
 })
 
