02b396624240653dc60461adb6cdc09989dfa0d6
sunnu16 <tjsvmfk@gmail.com>
2022-08-07 23:24:22 +0000

    mysql setting 2

--- App_mysql/example.sql
+++ App_mysql/example.sql
@@ -14,9 +14,9 @@ CREATE TABLE `author` (
 -- Dumping data for table `author`
 --
  
-INSERT INTO `author` VALUES (1,'egoing','developer');
-INSERT INTO `author` VALUES (2,'duru','database administrator');
-INSERT INTO `author` VALUES (3,'taeho','data scientist, developer');
+INSERT INTO `author` VALUES (1,'HaengUn','developer');
+INSERT INTO `author` VALUES (2,'chureu','database administrator');
+INSERT INTO `author` VALUES (3,'rich','data scientist, developer');
  
 --
 -- Table structure for table `topic`
@@ -35,8 +35,8 @@ CREATE TABLE `topic` (
 -- Dumping data for table `topic`
 --
  
-INSERT INTO `topic` VALUES (1,'MySQL','MySQL is...','2018-01-01 12:10:11',1);
-INSERT INTO `topic` VALUES (2,'Oracle','Oracle is ...','2018-01-03 13:01:10',1);
-INSERT INTO `topic` VALUES (3,'SQL Server','SQL Server is ...','2018-01-20 11:01:10',2);
-INSERT INTO `topic` VALUES (4,'PostgreSQL','PostgreSQL is ...','2018-01-23 01:03:03',3);
-INSERT INTO `topic` VALUES (5,'MongoDB','MongoDB is ...','2018-01-30 12:31:03',1);
+INSERT INTO `topic` VALUES (1,'MySQL','MySQL is...','2022-08-01 07:10:11',1);
+INSERT INTO `topic` VALUES (2,'Oracle','Oracle is ...','2022-08-03 14:01:10',1);
+INSERT INTO `topic` VALUES (3,'SQL Server','SQL Server is ...','2022-08-20 11:01:10',2);
+INSERT INTO `topic` VALUES (4,'PostgreSQL','PostgreSQL is ...','2022-08-23 01:03:03',3);
+INSERT INTO `topic` VALUES (5,'MongoDB','MongoDB is ...','2022-08-30 12:31:03',1);
