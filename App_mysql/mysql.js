c544e48c4e1b67f3803f34332be67fee738a70cc
sunnu16 <tjsvmfk@gmail.com>
2022-08-09 13:32:22 +0000

    mysql setting3

---
+++ App_mysql/mysql.js
@@ -0,0 +1,23 @@
+//mysql 모듈 사용 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+
+    host : '15.164.226.103', //DB 서버가 어디에 있는가
+    user : 'root', 
+    password : '0000',
+    database : 'app_set1' //연결할 database name
+
+});
+
+connection.connect(); // mysql 접속 
+
+//query를 통해 DB의 topic 테이블 (객체의 형태) 반환
+connection.query('SELECT * FROM topic', function(error, results, fields){
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+
+});
+
+connection.end();
