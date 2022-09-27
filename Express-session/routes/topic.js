7f681199b6a9008f781e64f096907e7e2957e627
sunnu16 <tjsvmfk@gmail.com>
2022-09-27 23:55:53 +0000

    Express-session access control

--- Express-session/routes/topic.js
+++ Express-session/routes/topic.js
@@ -18,6 +18,13 @@ var auth = require('../lib/auth.js');
 
 // /create
 router.get('/create', function(request, response){
+
+  //접근 제어
+  if(!auth.IsOwner(request, response)){ //소유자가 아니라면 홈으로 튕기게
+
+    response.redirect(`/`); 
+    return false; //false를 사용하여 다음으로 넘어가지 않게 끊기
+  }
                     
   var title = 'Create';
   var list = template.List(request.list); //topics 함수 불러오기
@@ -46,41 +53,13 @@ router.get('/create', function(request, response){
 // /create_process
 router.post('/create_process', function(request, response){
   
-  /*
-  var body = '';
-
-  //request.on을 사용하여 data 수신할때마다 function(data){}를 호출
-  request.on('data', function(data){  
-
-      body = body + data;
-        body에다 callback이 실행될 때마다 data를 추가
-      (+전송된 data의 크기가 너무 클때, 
-      접속을 끊을 보안 장치도 추가 가능한 방법도 존재함을 인지)
-
-  });
-
-  //data 수신이 끝났을때
-  request.on('end', function(){
-              
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-
-    fs.writeFile(`data/${title}`, description, 'utf8', function(error){
-      response.writeHead(302, {Location: `/?id=${title}`});
-      response.end();
-
-    })     
-
-  }); 
-  */
-
+  
   //접근 제어
-  if(authIsOwner(request, response) === false){
+  if(!auth.IsOwner(request, response)){ //소유자가 아니라면 홈으로 튕기게
 
-    response.end('Login Required!!');
-    return false;
-  } //로그인 상태가 아니면 다음으로 넘어가지 않도록
+    response.redirect(`/`); 
+    return false; //false를 사용하여 다음으로 넘어가지 않게 끊기
+  }
 
   var post = request.body;
   var title = post.title;
@@ -94,11 +73,45 @@ router.post('/create_process', function(request, response){
 
   })
 
+/*
+  var body = '';
+
+  //request.on을 사용하여 data 수신할때마다 function(data){}를 호출
+  request.on('data', function(data){  
+
+      body = body + data;
+        body에다 callback이 실행될 때마다 data를 추가
+      (+전송된 data의 크기가 너무 클때, 
+      접속을 끊을 보안 장치도 추가 가능한 방법도 존재함을 인지)
+  });
+
+  //data 수신이 끝났을때
+  request.on('end', function(){
+              
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+
+    fs.writeFile(`data/${title}`, description, 'utf8', function(error){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+    })     
+  }); 
+*/
+
+
 });
 
 
 // /update
 router.get('/update/:pageId', function(request, response){
+
+  //접근 제어
+  if(!auth.IsOwner(request, response)){ //소유자가 아니라면 홈으로 튕기게
+
+    response.redirect(`/`); 
+    return false; //false를 사용하여 다음으로 넘어가지 않게 끊기
+  }
   
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(error, description){
@@ -140,49 +153,13 @@ router.get('/update/:pageId', function(request, response){
 // /update_process
 router.post('/update_process', function(request, response){
 
-  /*
-  var body = '';
-  request.on('data', function(data){
-    
-    body = body + data;
-      body에다 callback이 실행될 때마다 data를 추가
-    (+전송된 data의 크기가 너무 클때, 
-    접속을 끊을 보안 장치도 추가 가능한 방법도 존재함을 인지) 
-
-  });
-
-  //data 수신이 끝났을때
-  request.on('end', function(){
-        
-    var post = qs.parse(body); //post에 정보가 입력
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-
-      fs.writeFile(`data/${title}`, description, 'utf8', function(error){
-        
-
-        //response.writeHead(302, {Location: `/?id=${title}`});
-        //response.end(); 
-
-        //express redirect
-        response.redirect(`/?id=${title}`);
-
-      });     
-
-    });  
-
-  });
-
-  */
 
   //접근 제어
-  if(authIsOwner(request, response) === false){
+  if(!auth.IsOwner(request, response)){ //소유자가 아니라면 홈으로 튕기게
 
-    response.end('Login Required!!');
-    return false;
-  } //로그인 상태가 아니면 다음으로 넘어가지 않도록
+    response.redirect(`/`); 
+    return false; //false를 사용하여 다음으로 넘어가지 않게 끊기
+  }
 
   var post = request.body; //post에 정보가 입력
   var id = post.id;
@@ -199,11 +176,52 @@ router.post('/update_process', function(request, response){
 
   });
 
+
+/*
+  var body = '';
+  request.on('data', function(data){
+    
+    body = body + data;
+      body에다 callback이 실행될 때마다 data를 추가
+    (+전송된 data의 크기가 너무 클때, 
+    접속을 끊을 보안 장치도 추가 가능한 방법도 존재함을 인지) 
+  });
+
+  //data 수신이 끝났을때
+  request.on('end', function(){
+        
+    var post = qs.parse(body); //post에 정보가 입력
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+
+      fs.writeFile(`data/${title}`, description, 'utf8', function(error){
+        
+        //response.writeHead(302, {Location: `/?id=${title}`});
+        //response.end(); 
+
+        //express redirect
+        response.redirect(`/?id=${title}`);
+
+      });     
+    });  
+  });
+*/
+
+
 });
 
 
 // /delete
 router.post('/delete_process', function(request, response){
+
+  //접근 제어
+  if(!auth.IsOwner(request, response)){ //소유자가 아니라면 홈으로 튕기게
+
+    response.redirect(`/`); 
+    return false; //false를 사용하여 다음으로 넘어가지 않게 끊기
+  }
   
   var post = request.body;
   var id = post.id;
